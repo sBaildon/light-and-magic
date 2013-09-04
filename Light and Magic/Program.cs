c06aed629dceced051286215827206eb5eb72f79
Sean <sean@sbaildon.com>
2013-09-04 12:36:52 +0100

    initial commit

---
+++ Light and Magic/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Presentation;
+using Microsoft.SPOT.Presentation.Controls;
+using Microsoft.SPOT.Presentation.Media;
+using Microsoft.SPOT.Touch;
+
+using Gadgeteer.Networking;
+using GT = Gadgeteer;
+using GTM = Gadgeteer.Modules;
+using Gadgeteer.Modules.GHIElectronics;
+
+namespace Light_and_Magic
+{
+
+    public partial class Program
+    {
+
+        void ProgramStarted()
+        {
+        }
+    }
+}
