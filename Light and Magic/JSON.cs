1ca12a3ab843320509eca8c93561c38083c76852
Sean Baildon <sean@sbaildon.com>
2014-02-08 14:49:45 +0000

    JSON.cs reorganise/rename

--- Light and Magic/JSON.cs
+++ Light and Magic/JSON.cs
@@ -1,23 +1,25 @@
 using System;
 using System.Collections;
+
 using Microsoft.SPOT;
+
 using Json.NETMF;
 
 namespace Light_and_Magic {
 	class JSON {
 		public static string Encode(Hashtable table) {
-			ArrayList datasets = new ArrayList();
+			ArrayList datastreams = new ArrayList();
 
 			foreach (DictionaryEntry entry in table) {
 				Hashtable dataset = new Hashtable();
 				dataset.Add("id", entry.Key.ToString());
 				dataset.Add("current_value", entry.Value.ToString());
-				datasets.Add(dataset);
-;			}
+				datastreams.Add(dataset);
+			}
 
 			Hashtable json = new Hashtable();
 			json.Add("version", "1.0.0");
-			json.Add("datastreams", datasets);
+			json.Add("datastreams", datastreams);
 
 			return JsonSerializer.SerializeObject(json);
 		}
